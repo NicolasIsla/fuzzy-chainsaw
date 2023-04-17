83eb68c29ccc6d05843ecaf1a090491040ce1f23
BuildTools <unconfigured@null.spigotmc.org>
2023-04-17 00:40:36 -0400

    test

--- Simulacion/Simulacion.py
+++ Simulacion/Simulacion.py
@@ -1,8 +1,11 @@
 """
 Módulo para crear simulación temporal
 """
+import sys    
+print("In module products sys.path[0], __package__ ==", sys.path[0], __package__)
 import numpy as np
 import matplotlib.pyplot as plt
+from ..utils.FuzzyOperations import u_A
 
 class Simulacion:
     """
@@ -15,23 +18,27 @@ class Simulacion:
         self.to = to
         self.N = int(duracion * frec_muestreo)
         self.tiempo = np.linspace(to, duracion+to, self.N)
-        self.resultado = np.zeros(self.N)
+        self.P = np.zeros(self.N)  # Idealmente es presión
     
-    # Ejecuta simulación básica.
-    def run_f(self):
+    # Ejecuta simulación básica sobre P.
+    def run_sim(self):
         for i, t in enumerate(self.tiempo):
-            self.resultado[i] = self.f(t+self.to)
+            self.P[i] = self.f(t+self.to)
 
     # Genera gráfico con resultados de simulación.
-    def plot_f(self):
+    def plot_sim(self, resultado=None):
         plt.figure(figsize=(7,5))
-        plt.plot(self.tiempo, self.resultado)
+        if resultado is not None: plt.plot(self.tiempo, resultado)
+        else: plt.plot(self.tiempo, self.P)
         plt.xlabel("Tiempo (segundos)")
-        plt.ylabel("f(t)")
+        plt.ylabel("Presión (Pa)")
         plt.title("Gráfico")
         plt.show()
 
 class Simulacion_CLD(Simulacion):
+    """
+    Crea simulación de un controlador difuso CLD.
+    """
     def __init__(self, 
                  duracion, 
                  frec_muestreo, 
@@ -54,7 +61,7 @@ class Simulacion_CLD(Simulacion):
         self.P_inicial = P_inicial
         self.metodo_desdifusion = metodo_desdifusion
 
-        mensaje = f"""Se creó el simulador para el controlador con los siguientes parámetros.
+        mensaje = f"""Se creó el simulador para el controlador difuso con los siguientes parámetros.
         duración = {duracion} [s]
         frec_muestreo = {frec_muestreo} [Hz]
         rango_EP = {rango_EP}
@@ -62,26 +69,47 @@ class Simulacion_CLD(Simulacion):
         rango_deltaH = {rango_EP}
 
         La variable a controlar es P={K}*H usando lógica difusa.
-        La presión objetivo es P_obj={P_obj} [Pa], y la presión inicial es P_inicial={P_obj} [Pa]
+        La presión objetivo es P_obj={P_obj}[Pa], y la presión inicial es P_inicial={P_obj}[Pa]
         """
         if verbose is True: print(mensaje)
 
     def definir_rangos(self, rango_EP, rango_TP, rango_deltaH):
         """
-        
+        Permite definir los rangos de operación del controlador difuso.
         """
         self.rango_EP = rango_EP
         self.rango_TP = rango_TP
         self.rango_deltaH = rango_deltaH
     
     def definir_presion_objetivo(self, P_obj):
+        """
+        Permite definir la presión objetivo.
+        """
         self.P_obj = P_obj
+    
+    def definir_metodo_desdifusion(self, metodo_desdifusion):
+        """
+        Permite definir el método de desdifusión.
+        """
+        self.metodo_desdifusion = metodo_desdifusion
+
+    # Override
+    def run_sim(self):
+        for i, t in enumerate(self.tiempo):
+            # Se aplica la lógica difusa sobre los valores
+            for i, (EP, TP) in enumerate(self.mapa_reglas.keys()):
+                EP_fuzzy = u_A
+    
+
 
 
 if __name__ == "__main__":
+    """
+    Como prueba se grafica la función seno.
+    """
     frec = 5  # Hz
     frec_muestreo = 100 # Hz
     f = lambda t: np.sin(2*np.pi*frec*t)
     sim = Simulacion_CLD(2, frec_muestreo, f)
-    sim.run_f()
-    sim.plot_f()
+    sim.run_sim()
+    sim.plot_sim()
